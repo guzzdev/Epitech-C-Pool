8b4aa7ceed7ac28e9115fa8016d643f9701d693d
Louis OLIVE <louis.olive@epitech.eu>
2021-10-06 20:53:53 +0200

    msg

--- cpoolday03/my_put_nbr.c
+++ cpoolday03/my_put_nbr.c
@@ -6,15 +6,21 @@
 */
 #include <unistd.h>
 
-void my_putchar(char c){
-    write(1, &c, 1);
-}
+int my_put_nbr(int nbr)
+{
+	int nbr2;
 
-int my_put_nbr(int nb){
-    return 0;
-}
-
-void main(void){
-    my_put_nbr(42);
-    my_put_nbr(-2147483647);
+	if (nbr < 0) {
+		my_putchar('-');
+		nbr = nbr * (-1);
+	}
+	if (nbr >= 10) {
+		nbr2 = nbr % 10;
+		nbr = nbr / 10;
+		my_put_nbr(nbr);
+		my_putchar(nbr2 + 48);
+	} else {
+		my_putchar(nbr + 48);
+	}
+	return (0);
 }
