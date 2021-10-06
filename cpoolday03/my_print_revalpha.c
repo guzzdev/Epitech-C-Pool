638e8f39f216f0f1592208b4aafddac3998e5ed8
Louis OLIVE <louis.olive@epitech.eu>
2021-10-06 11:24:45 +0200

    revalpha

---
+++ cpoolday03/my_print_revalpha.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** my_print_revalpha
+** File description:
+** Print the alphabet in descending order
+*/
+
+int my_print_revalpha(void)
+{
+    int i;
+    
+    for (i = 122; i != 96; i--) {
+        my_putchar(i);
+    }
+    return (0);
+}
+
