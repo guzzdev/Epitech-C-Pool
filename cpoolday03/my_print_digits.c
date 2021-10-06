8b6e7082911f201204b6297f0baf6a26f2175722
Louis OLIVE <louis.olive@epitech.eu>
2021-10-06 11:57:37 +0200

    digits

---
+++ cpoolday03/my_print_digits.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** my_print_digits
+** File description:
+** Prints all digits
+*/
+
+my_print_digits(void)
+{
+    int i;
+    for(i=48;i<58;i++){
+        my_putchar(i);
+    }
+    return (0);
+}
