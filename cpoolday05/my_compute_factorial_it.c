02adfdf7bb7be9686282859c41f3e6c0ce2919e1
Louis OLIVE <louis.olive@epitech.eu>
2021-10-08 15:15:37 +0200

    msg

--- cpoolday05/my_compute_factorial_it.c
+++ cpoolday05/my_compute_factorial_it.c
@@ -2,19 +2,20 @@
 ** EPITECH PROJECT, 2021
 ** my_compute_factorial_it
 ** File description:
-** Iterative function that returns the factorial of the number given as a parameter
+** Iterative function that returns the factorial of the number given
 */
 
-int my_compute_factorial_it(int nb){
+int my_compute_factorial_it(int nb)
+{
     int result = 1;
     int i;
 
-    if (nb <= 0){
+    if (nb < 0)
         return 0;
-    }
+    if (nb == 0)
+        return 1;
     for (i = nb; i >= 1; i--) {
         result = result * i;
     }
-
     return result;
 }
