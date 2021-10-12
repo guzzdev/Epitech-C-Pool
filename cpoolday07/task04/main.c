9e8490e2941cb7eb57f20750cf5cb720b66e1f64
Louis OLIVE <louis.olive@epitech.eu>
2021-10-12 15:01:40 +0200

    msg

---
+++ cpoolday07/task04/main.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** main.c
+** File description:
+** main
+*/
+
+int my_putstr(char const *str)
+{
+    int a;
+
+    a=0;
+    while (str[a] != '\0') {
+        my_putchar(str[a]);
+        a++;
+    }
+    my_putchar(10);
+}
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < argc; i++) {
+        my_putstr(argv[i]);
+    }
+    return 0;
+}
