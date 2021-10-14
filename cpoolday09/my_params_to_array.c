2dedee6c9a9a210f1bdd115ff39d4e8552e8ca87
Louis OLIVE <louis.olive@epitech.eu>
2021-10-14 23:00:41 +0200

    msg

--- cpoolday09/my_params_to_array.c
+++ cpoolday09/my_params_to_array.c
@@ -5,14 +5,6 @@
 ** my_params_to_array
 */
 
-struct info_param
-{
-    int length; // parameter 's length
-    char *str; // parameter 's address
-    char *copy; // parameter 's copy
-    char ** word_array; // the result of my_str_to_word_array(str)
-};
-
 struct info_param *my_params_to_array(int ac, char **av)
 {
     struct info_param *result;
