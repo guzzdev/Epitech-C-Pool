76d8ffb11c903d06bcd38e58c6678771366b1a55
Louis OLIVE <louis.olive@epitech.eu>
2021-10-15 16:24:47 +0200

    Makefile

---
+++ cpoolday10/lib/my/my_strcat.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** my_strcat
+** File description:
+** my_strcat
+*/
+
+char *my_strcat(char *dest , char const *src)
+{
+    int dest_lenght;
+    int i;
+
+    for (dest_lenght = 0; dest[dest_lenght] != '\0'; dest_lenght++);
+    for (i = 0; src[i] != '\0'; i++)
+        dest[dest_lenght + i] = src[i];
+    dest[dest_lenght + i] = '\0';
+    return (dest);
+}
---
+++ cpoolday10/lib/my/my_strncpy.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2021
+** my_strncpy
+** File description:
+** my_strncpy
+*/
+
+char *my_strncpy(char *dest, char const *src, int n)
+{
+    return 0;
+}
