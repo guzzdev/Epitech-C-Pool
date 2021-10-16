da503a20155fc9c2f5de952a12aa89b719cc0855
Adam Balfet <adam.balfet@epitech.eu>
2021-10-16 04:38:49 -0400

    step one

--- rush2/lib/my/rush_step_one.c
+++ rush2/lib/my/rush_step_one.c
@@ -8,12 +8,13 @@
 int rush_step_one(int argc, char *const *argv)
 {
     int count = 0;
-    char *sentence = argv[1]
+    char *sentence = argv[1];
 
     if (argc <= 1)
         return 84;
     for (int i = 0; argv[1][i] != '\0'; ++i;) {
-        if (sentence[i] == argv[2][0])
+        if (sentence[i] == argv[2][0] || sentence[i] == argv[2][0] - 32
+            || sentence[i] == argv[2][0] + 32)
             ++count;
     }
     return 0;
