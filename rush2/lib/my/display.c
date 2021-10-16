2059c9dca50bd41ab26005748df414d43cdd35a9
Louis OLIVE <louis.olive@epitech.eu>
2021-10-16 10:50:10 +0200

    Makefile

--- rush2/lib/my/display.c
+++ rush2/lib/my/display.c
@@ -1,6 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** display
+** File description:
+** display
+*/
+
 int display(char const *argv[], int count)
 {
     my_putchar(argv[2][0]);
     my_putchar(':');
-    my_putnbr(count);
+    my_put_nbr(count);
 }
