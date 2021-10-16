855d3de74ecba88fcf6b22b5e00697b7ebf1826a
Adam Balfet <adam.balfet@epitech.eu>
2021-10-16 03:58:41 -0400

    main

---
+++ rush2/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** main
+** File description:
+** main
+*/
+
+void main(void)
+{
+    char *sentence = "bonjour je suis francais et pas allemand";
+
+    rush2(sentence);
+    return;
+}
