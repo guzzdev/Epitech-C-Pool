d87cc3c88095cf87f12b74bd0da23743e4245c25
Louis OLIVE <louis.olive@epitech.eu>
2021-10-30 14:47:07 +0200

    final push

--- finalstumper/final_stumper.c
+++ finalstumper/final_stumper.c
@@ -5,54 +5,91 @@
 ** final_stumper
 */
 
-void display(char * str, int lenght, int height)
+/**
+** Returns the number of columns
+**
+** @param buff array of given square
+*/
+int get_col_nbr(char * buff)
+{
+    int nbr_col = 0;
+
+    for (; buff[nbr_col] != '\n'; nbr_col++);
+    return nbr_col;
+}
+
+/**
+** Returns the number of lines
+**
+** @param buff array of given square
+*/
+int get_line_nbr(char * buff)
+{
+    int nbr_line = 0;
+
+    for (int i = 0; buff[i] != '\0'; i++) {
+        if (buff[i] == '\n')
+            nbr_line++;
+    }
+    return nbr_line;
+}
+
+/**
+** Display the result
+**
+** @param str assignement title
+** @param nbr_col number of columns
+** @param nbr_line number of lines
+*/
+void display(char * str, int nbr_col, int nbr_line)
 {
     my_putchar('[');
     my_putstr(str);
     my_putstr("] ");
-    my_put_nbr(lenght);
+    my_put_nbr(nbr_col);
     my_putchar(' ');
-    my_put_nbr(height);
+    my_put_nbr(nbr_line);
 }
 
-void display_or(char * buff, int col, int line)
+/**
+** Display multiple results
+**
+** @param buff array of given square
+** @param nbr_col number of columns
+** @param nbr_line number of lines
+*/
+void display_or(char * buff, int nbr_col, int nbr_line)
 {
-    display("rush1-3", col, line);
+    display("rush1-3", nbr_col, nbr_line);
     my_putstr(" || ");
-    display("rush1-4", col, line);
+    display("rush1-4", nbr_col, nbr_line);
     my_putstr(" || ");
-    display("rush1-5", col, line);
+    display("rush1-5", nbr_col, nbr_line);
 }
 
-void display_handler(char * buff, int col, int line)
-{
-    int last_line = (col * (line-1)) + line - 1;
-    int last_char = ((col * line) + line) - 2;
-
-    if (buff[0] == 'o')
-        display("rush1-1", col, line);
-    if (buff[0] == '*' || buff[0] == '/')
-        display("rush1-2", col, line);
-    if (buff[0] == 'A' && buff[col-1] == 'A')
-        display("rush1-3", col, line);
-    if (buff[0] == 'B')
-        display_or(buff, col, line);
-    if (buff[last_line] == 'A' && buff[last_char] == 'C')
-        display("rush1-4", col, line);
-    else
-        display("rush1-5", col, line);
-}
-
+/**
+** Test the given square
+**
+** @param buff array of given square
+*/
 void final_stumper(char *buff)
 {
-    int col = 0;
-    int line = 0;
+    int nbr_col = get_col_nbr(buff);
+    int nbr_line = get_line_nbr(buff);
+    int last_line = (nbr_col * (nbr_line-1)) + nbr_line - 1;
+    int last_char = ((nbr_col * nbr_line) + nbr_line) - 2;
 
-    for(; buff[col] != '\n'; col++);
-    for(int i = 0; buff[i] != '\0'; i++) {
-        if (buff[i] == '\n')
-            line++;
-    }
-    display_handler(buff, col, line);
+    if (buff[0] == 'o')
+        display("rush1-1", nbr_col, nbr_line);
+    else if (buff[0] == '*' || buff[0] == '/')
+        display("rush1-2", nbr_col, nbr_line);
+    else if (buff[0] == 'A' && buff[nbr_col-1] == 'A')
+        display("rush1-3", nbr_col, nbr_line);
+    else if (buff[0] == 'B')
+        display_or(buff, nbr_col, nbr_line);
+    else if (buff[last_line] == 'A' && buff[last_char] == 'C')
+        display("rush1-4", nbr_col, nbr_line);
+    else
+        display("rush1-5", nbr_col, nbr_line);
     my_putchar('\n');
 }
--- finalstumper/main.c
+++ finalstumper/main.c
@@ -10,6 +10,19 @@
 #include "include/my.h"
 #include "include/final_stumper.h"
 
+int error_handler(char * buff, int len)
+{
+     if (len < 0)
+          return (84);
+     for (int i = 0; buff[i] != '\0'; i++) {
+          if (buff[i] != 65 && buff[i] != 66 && buff[i] != 67
+          && buff[i] != 111 && buff[i] != 42 && buff[i] != 45
+          && buff[i] != 47 && buff[i] != 92 && buff[i] != 124 
+          && buff[i] != 10 && buff[i] != 0 && buff[i] != 32)
+               return 84;
+     }
+}
+
 int main()
 {
      char buff[BUFF_SIZE + 1];
@@ -19,8 +32,8 @@ int main()
           offset = offset + len;
      }
      buff[offset] = '\0';
-     if (len < 0)
-          return (84);
+     if (error_handler(buff, len) == 84)
+          return 84;
      final_stumper(buff);
-     return (0) ;
+     return (0);
 }
