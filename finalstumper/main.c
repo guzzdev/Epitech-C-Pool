b0788f16e8ab513a010fde6f1eceb5aa38be691c
Louis OLIVE <louis.olive@epitech.eu>
2021-10-30 10:29:00 +0200

    added assignement helps

--- finalstumper/main.c
+++ finalstumper/main.c
@@ -5,8 +5,19 @@
 ** main
 */
 
-int main(int argc, char const *argv[])
+#include <unistd.h>
+#define BUFF_SIZE (4096)
+
+int main()
 {
-    
-    return 0;
-}
+     char buff[BUFF_SIZE + 1];
+     int offset = 0;
+     int len ;
+     while ((len = read(0 , buff + offset , BUFF_SIZE - offset)) > 0) {
+          offset = offset + len ;
+     }
+     buff[offset] = '\0';
+     if (len < 0)
+          return (84) ;
+     rush3(buff) ;
+     return (0) ;
