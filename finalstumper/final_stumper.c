b1a12a16851e6fa14e0cdcbddb2ef38f3f3daef2
Louis OLIVE <louis.olive@epitech.eu>
2021-10-30 13:59:55 +0200

    merged

--- finalstumper/final_stumper.c
+++ finalstumper/final_stumper.c
@@ -5,7 +5,7 @@
 ** final_stumper
 */
 
-int display(char * str, int lenght, int height)
+void display(char * str, int lenght, int height)
 {
     my_putchar('[');
     my_putstr(str);
@@ -15,7 +15,7 @@ int display(char * str, int lenght, int height)
     my_put_nbr(height);
 }
 
-void display_or(char buff, int col, int line)
+void display_or(char * buff, int col, int line)
 {
     display("rush1-3", col, line);
     my_putstr(" || ");
@@ -24,20 +24,20 @@ void display_or(char buff, int col, int line)
     display("rush1-5", col, line);
 }
 
-void display_handler(char *buff, int col, int line)
+void display_handler(char * buff, int col, int line)
 {
     int last_line = (col * (line-1)) + line - 1;
     int last_char = ((col * line) + line) - 2;
 
     if (buff[0] == 'o')
         display("rush1-1", col, line);
-    else if (buff[0] == '*' || buff[0] == '/')
+    if (buff[0] == '*' || buff[0] == '/')
         display("rush1-2", col, line);
-    else if (buff[0] == 'A' && buff[col-1] == 'A')
+    if (buff[0] == 'A' && buff[col-1] == 'A')
         display("rush1-3", col, line);
-    else if (buff[0] == 'B')
+    if (buff[0] == 'B')
         display_or(buff, col, line);
-    else if (buff[last_line] == 'A' && buff[last_char] == 'C')
+    if (buff[last_line] == 'A' && buff[last_char] == 'C')
         display("rush1-4", col, line);
     else
         display("rush1-5", col, line);
