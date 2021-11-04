cd2f95c14b3201d7809623a0a3f21a4f55975364
Theo <theo.d-amore@epitech.eu>
2021-11-04 19:25:23 -0400

    Error handling

---
+++ bistromatic/error_handling/error_handling.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2021
+** error_handling.c
+** File description:
+** error handling file 1 of the bistro-matic
+*/
+
+int missing_operator(char **str)
+{
+    int i = 0;
+    int bool = 1;
+
+    while (str[0][i] != '\0') {
+        if (str[0][i] == '+' || str[0][i] == '-' || str[0][i] == '*')
+            bool = 0;
+        if (str[0][i] == '/' || str[0][i] == '%' || str[0][i] == '^')
+            bool = 0;
+        i++;
+    }
+    if (bool == 1) {
+        my_putstr("syntax error");
+        exit (84);
+    }
+    if (!bool)
+        return (0);
+}
